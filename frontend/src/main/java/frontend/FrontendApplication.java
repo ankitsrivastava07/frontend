88f7082fdcbe6f8e6c70d090e3067cfe4a413c7b
qtpl0036 <ravish.rathi@qtsolv.com>
2021-06-14 01:39:01 +0530

    change

--- frontend/src/main/java/frontend/FrontendApplication.java
+++ frontend/src/main/java/frontend/FrontendApplication.java
@@ -3,6 +3,9 @@ package frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.Logger;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,11 @@ public class FrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 
 }
