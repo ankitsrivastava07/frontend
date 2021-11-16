0ae2bb73da1fffd4614f8258c74f2b9d12df8d89
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2021-11-16 14:46:05 +0530

    Revert "changes"
    This reverts commit 821b3fdd87fae16b60b6d5d02cb1f501b0a42cd8.

--- frontend/src/main/java/frontend/controller/HomeController.java
+++ frontend/src/main/java/frontend/controller/HomeController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.UserAgent;
 import frontend.api.dto.response.UserDto;
 import frontend.api.request.ChangePasswordReqest;
 import frontend.api.request.CreateUserRequestDto;
@@ -124,8 +126,8 @@ public class HomeController {
 
 	@GetMapping("/signout")
 	public void signout(HttpServletRequest request,HttpServletResponse response) throws IOException {
-		String token=frontendService.getToken(request);
-		TokenStatus tokenStatus=frontendService.invalidateToken(token);
+		TokenStatus tokenStatus=frontendService.isValidToken(request,response);
+		tokenStatus=frontendService.invalidateToken(tokenStatus.getAccessToken());
 		response.sendRedirect("/signin");
 	}
 
@@ -141,6 +143,7 @@ public class HomeController {
 
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@RequestBody CreateUserRequestDto createUserRequestDto,HttpServletRequest request,HttpServletResponse response) {
+
 		String browser=UUID.randomUUID().toString();
 		createUserRequestDto.setBrowser(browser);
 		CreateUserResponseStatus status = frontendService.register(createUserRequestDto);
