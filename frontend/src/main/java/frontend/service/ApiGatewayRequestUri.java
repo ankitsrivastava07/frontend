0b75c75956149648c1e498dca1440e694a270e98
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2021-11-26 22:05:16 +0530

    changes

--- frontend/src/main/java/frontend/service/ApiGatewayRequestUri.java
+++ frontend/src/main/java/frontend/service/ApiGatewayRequestUri.java
@@ -19,7 +19,7 @@ import frontend.dto.AddToCartRequest;
 import frontend.response.AddToCartResponse;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "cloud-gateway-spring", url = "http://localhost:8765")
+@FeignClient(name = "cloud-gateway-spring", url = "http://cloud-gateway-spring.herokuapp.com/")
 public interface ApiGatewayRequestUri {
 
 	@PostMapping("/users/login")
