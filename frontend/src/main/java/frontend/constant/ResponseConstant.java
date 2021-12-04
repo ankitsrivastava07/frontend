a2e3c94a72e86247a24aa7639718c82703db18a0
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2021-12-04 18:55:41 +0530

    changes

--- frontend/src/main/java/frontend/constant/ResponseConstant.java
+++ frontend/src/main/java/frontend/constant/ResponseConstant.java
@@ -13,5 +13,7 @@ public class ResponseConstant {
     private String accessToken;
     private String browser;
     private Integer httpStatus=200;
+    public static final String UNAUTHORIZE_REQUEST_DEAULT_MESSAGE="Un Authorize Request";
     public static final String FILE_EXTENSION_VALIDATION_FAILED_DEFAULT_MESSAGE="Invalid file format given, allow formats jpg|png|jpeg|gif";
+    public static final String FILE_LIMIT_EXCEED_DEAULT_MESSAGE="File size should be less than 10 MB";
 }
