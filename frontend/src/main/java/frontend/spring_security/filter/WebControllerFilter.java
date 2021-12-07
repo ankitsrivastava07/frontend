fc27bac57fda622af92cd1916dbf945f4bd71e32
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2021-12-07 13:51:03 +0530

    changes

--- frontend/src/main/java/frontend/spring_security/filter/WebControllerFilter.java
+++ frontend/src/main/java/frontend/spring_security/filter/WebControllerFilter.java
@@ -27,28 +27,28 @@ public class WebControllerFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
       HttpServletRequest request1= (HttpServletRequest)request;
       HttpServletResponse response1 = (HttpServletResponse)response;
-        String jwtToken=request1.getHeader("Authentication");
         String uri=request1.getServletPath();
         Cookie cookies[]=request1.getCookies();
-        /*if(jwtToken!=null) {
-            TokenStatus tokenStatus = frontendService.isValidToken(jwtToken);
-            TenantContext.setTokenStatus(tokenStatus);
-            chain.doFilter(request,response);
-            return;
-        }*/
+
         if(cookies!=null)
-            for(Cookie cookie:cookies)
-                if(cookie.getName().equalsIgnoreCase("session_Token")) {
-                    TokenStatus tokenStatus = frontendService.isValidToken(cookie.getValue());
-                    TenantContext.setTokenStatus(tokenStatus);
-                    chain.doFilter(request,response);
+            for(Cookie cookie:cookies) {
+                if (cookie.getName().equalsIgnoreCase("session_Token")) {
+                    isValidToken(cookie.getValue());
+                    chain.doFilter(request, response);
                     return;
                 }
+            }
         chain.doFilter(request,response);
     }
 
     @Override
     public void destroy() {
         Filter.super.destroy();
+    }
+
+    private boolean isValidToken(String jwtToken){
+        TokenStatus tokenStatus = frontendService.isValidToken(jwtToken);
+        TenantContext.setTokenStatus(tokenStatus);
+        return tokenStatus.isStatus();
     }
 }
