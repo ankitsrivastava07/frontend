44dc67841acc2df022c8d583babd31cdd79fe1f2
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2022-02-15 13:18:57 +0530

    changes

--- frontend/src/main/java/frontend/constant/ResponseConstant.java
+++ frontend/src/main/java/frontend/constant/ResponseConstant.java
@@ -10,7 +10,7 @@ public class ResponseConstant {
     private String browser;
     private Integer httpStatus=200;
     public static final String UNAUTHORIZE_REQUEST_DEAULT_MESSAGE="Un Authorize Request";
-    public static final String FILE_EXTENSION_VALIDATION_FAILED_DEFAULT_MESSAGE="Invalid file format given, allow formats jpg|png|jpeg|gif";
+    public static final String FILE_EXTENSION_VALIDATION_FAILED_DEFAULT_MESSAGE="Invalid file format given, allow formats jpg , png , jpeg , gif";
     public static final String FILE_LIMIT_EXCEED_DEAULT_MESSAGE="File size should be less than 10 MB";
     public static final String SESSION_EXPIRED_DEFAULT_MESSAGE="Your session has been expired.Please logined again";
 }
