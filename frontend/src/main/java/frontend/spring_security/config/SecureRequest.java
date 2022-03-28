37aa4831d9d4b5b2bf39b76b393e4e2df44a07d3
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2022-03-29 00:26:32 +0530

    changes

--- frontend/src/main/java/frontend/spring_security/config/SecureRequest.java
+++ frontend/src/main/java/frontend/spring_security/config/SecureRequest.java
@@ -14,6 +14,9 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableGlobalAuthentication
@@ -71,6 +74,17 @@ public class SecureRequest extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationEntryPointFilter authentionEntryPoint() throws Exception {
         return new AuthenticationEntryPointFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "OPTIONS" , "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
    /* @Override
