9060a48080e3be9b98aca3ee189d5ca8d8807e3f
Ankit Srivastava <ankitsrivastava7835@gmail.com>
2022-09-11 21:12:13 +0530

    cors policy added

--- frontend/src/main/java/frontend/spring_security/config/SecureRequest.java
+++ frontend/src/main/java/frontend/spring_security/config/SecureRequest.java
@@ -1,4 +1,5 @@
 package frontend.spring_security.config;
+
 import frontend.service.ApiGatewayRequestUri;
 import frontend.service.FrontendService;
 import frontend.session_validator.JwtAccessTokenUtil;
@@ -22,27 +23,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableGlobalAuthentication
 @EnableWebSecurity(debug = true)
 public class SecureRequest extends WebSecurityConfigurerAdapter {
-    @Autowired private ApiGatewayRequestUri apiGatewayRequestUri;
-    @Autowired private AuthenticateUser authenticateUser;
-    @Autowired private AuthenticationEntryPointFilter authenticationEntryPointFilter;
-    @Autowired JwtAccessTokenUtil jwtAccessTokenUtil;
+    @Autowired
+    private ApiGatewayRequestUri apiGatewayRequestUri;
+    @Autowired
+    private AuthenticateUser authenticateUser;
+    @Autowired
+    private AuthenticationEntryPointFilter authenticationEntryPointFilter;
+    @Autowired
+    JwtAccessTokenUtil jwtAccessTokenUtil;
     @Autowired
     FrontendService frontendService;
+
     @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception{
+    public void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.httpBasic().authenticationEntryPoint(authenticationEntryPointFilter)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/")
                 .permitAll()
-                .antMatchers("/home","/book/*","/chat/**","/ws/**").permitAll()
+                .antMatchers("/home", "/book/*", "/chat/**", "/ws/**").permitAll()
                 .antMatchers("/popup").permitAll()
                 .antMatchers("/signout").permitAll()
                 .antMatchers("/user/validate").permitAll()
                 .antMatchers("/signout-from-all-devices")
                 .permitAll()
-                .antMatchers("/sign-up","/forget-password")
+                .antMatchers("/sign-up", "/forget-password")
                 .permitAll()
                 .antMatchers("/check-connection").permitAll()
                 .antMatchers("/ajax/*").permitAll()
@@ -51,19 +57,22 @@ public class SecureRequest extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/v1/user/profile/*")
                 .permitAll()
-                .antMatchers("/change-password","/change-password?code=")
+                .antMatchers("/change-password", "/change-password?code=")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().cors().disable().csrf().disable();
-                 httpSecurity.authenticationProvider(authenticateUser);
+                .and()
+                .csrf().disable();
+        httpSecurity.cors();
+        httpSecurity.authenticationProvider(authenticateUser);
     }
+
     @Override
     public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring().
-         antMatchers( "/images/**","/css/**","/ecommerce/**","/login-form/**","/register-form/**","/response-popup/css/**","/update/**","app.js");
+                antMatchers("/images/**", "/css/**", "/ecommerce/**", "/login-form/**", "/register-form/**", "/response-popup/css/**", "/update/**", "app.js");
     }
 
     @Bean
@@ -81,7 +90,7 @@ public class SecureRequest extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "OPTIONS" , "PUT", "DELETE").allowedOrigins("*")
+                registry.addMapping("/**").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE").allowedOrigins("*")
                         .allowedHeaders("*");
             }
         };
